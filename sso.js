0250e42aef76a0c8716d8dd8b001fc55f973e12b
@andrew.h <andrew.h@kitrum.com>
2024-02-16 18:53:13 +0200

    Updated sso.js

--- sso.js
+++ sso.js
@@ -1,128 +1,131 @@
-let data
-const iframe = document.createElement('iframe')
-iframe.id = 'inlineFrameExample'
-iframe.width = 0
-iframe.height = 0
-iframe.loading = 'lazy'
-iframe.style.display = 'none'
-iframe.src = 'https://platform.unbench.us'
-document.body.appendChild(iframe)
+if (window.innerWidth > 768) {
+  let data
+  const iframe = document.createElement('iframe')
+  iframe.id = 'inlineFrameExample'
+  iframe.width = 0
+  iframe.height = 0
+  iframe.style.display = 'none'
+  iframe.src = 'https://platform.unbench.us'
+  document.body.appendChild(iframe)
 
-const CrossOriginLocalStorage = function (currentWindow, iframe, onMessage) {
-  let childWindow
-  try {
-    childWindow = iframe.contentWindow
-  } catch (e) {
-    childWindow = iframe.contentWindow
-  }
-
-  currentWindow.onmessage = (event) => {
-    if (typeof event.data !== 'string') {
-      return
+  const CrossOriginLocalStorage = function (currentWindow, iframe, onMessage) {
+    let childWindow
+    try {
+      childWindow = iframe.contentWindow
+    } catch (e) {
+      childWindow = iframe.contentWindow
     }
-    return onMessage(JSON.parse(event.data))
-  }
 
-  this.getData = (key) => {
-    const messageData = {
-      key: key,
-      method: 'get',
-    }
-    this.postMessage(messageData)
-  }
-
-  this.setData = (key, data) => {
-    const messageData = {
-      key: key,
-      method: 'set',
-      data: data,
-    }
-    this.postMessage(messageData)
-  }
-
-  this.removeData = (key) => {
-    const messageData = {
-      key: key,
-      method: 'remove',
-    }
-    this.postMessage(messageData)
-  }
-
-  this.postMessage = (messageData) => {
-    childWindow.postMessage(JSON.stringify(messageData), '*')
-  }
-}
-
-let cross
-
-const onMessage = (payload) => {
-  switch (payload.method) {
-    case 'get':
-      data = payload
-      if (document.readyState !== 'complete') {
-        document.addEventListener('DOMContentLoaded', appendAccount)
-      } else {
-        if (!payload['data']) {
-          return
-        }
-        appendAccount()
+    currentWindow.onmessage = (event) => {
+      if (typeof event.data !== 'string') {
+        return
       }
-      break
-    default:
-      console.error('Unknown method "' + payload.method + '"', payload)
+      return onMessage(JSON.parse(event.data))
+    }
+
+    this.getData = (key) => {
+      const messageData = {
+        key: key,
+        method: 'get',
+      }
+      this.postMessage(messageData)
+    }
+
+    this.setData = (key, data) => {
+      const messageData = {
+        key: key,
+        method: 'set',
+        data: data,
+      }
+      this.postMessage(messageData)
+    }
+
+    this.removeData = (key) => {
+      const messageData = {
+        key: key,
+        method: 'remove',
+      }
+      this.postMessage(messageData)
+    }
+
+    this.postMessage = (messageData) => {
+      childWindow.postMessage(JSON.stringify(messageData), '*')
+    }
   }
-}
 
-const iframe = document.getElementById('inlineFrameExample')
-cross = new CrossOriginLocalStorage(window, iframe, onMessage)
-cross.getData('token')
+  let cross
 
-window.addEventListener('focus', function () {
-  cross.getData('token')
-})
+  window.onload = () => {
+    console.log('Uploaded SSO.js')
+    const onMessage = (payload) => {
+      switch (payload.method) {
+        case 'get':
+          data = payload
+          if (document.readyState !== 'complete') {
+            document.addEventListener('DOMContentLoaded', appendAccount)
+          } else {
+            if (!payload['data']) {
+              return
+            }
+            appendAccount()
+          }
+          break
+        default:
+          console.error('Unknown method "' + payload.method + '"', payload)
+      }
+    }
 
-const removeToken = (e) => {
-  cross.removeData('token')
-  window.open('https://platform.unbench.us/signin', '_self')
-}
+    const iframe = document.getElementById('inlineFrameExample')
+    cross = new CrossOriginLocalStorage(window, iframe, onMessage)
+    cross.getData('token')
 
-const appendAccount = () => {
+    window.addEventListener('focus', function () {
+      cross.getData('token')
+    })
+  }
 
-  document.querySelectorAll('.sign-up').forEach(i => i.remove())
-  document.querySelector('.hero-btn-wrapper').remove()
+  const removeToken = (e) => {
+    cross.removeData('token')
+    window.open('https://platform.unbench.us/signin', '_self')
+  }
 
-  document.querySelectorAll('.log-in').forEach(i => {
-    i.querySelector('.subtitle').innerHTML = 'Account'
-    i.classList.add('account-btn')
-    i.href = 'https://platform.unbench.us/account'
-  })
+  const appendAccount = () => {
 
-  const menus = document.querySelectorAll('.menu-btn-items')
-  const accountBtnWrapper = document.createElement('div')
-  accountBtnWrapper.classList.add('account-btn-wrapper')
+    document.querySelectorAll('.sign-up').forEach(i => i.remove())
+    document.querySelector('.hero-btn-wrapper').remove()
 
-  const mobileMenuBtn = document.createElement('div')
-  mobileMenuBtn.classList.add('account-btn-wrapper')
-  mobileMenuBtn.style.flexGrow = 1
-  mobileMenuBtn.innerHTML = `
+    document.querySelectorAll('.log-in').forEach(i => {
+      i.querySelector('.subtitle').innerHTML = 'Account'
+      i.classList.add('account-btn')
+      i.href = 'https://platform.unbench.us/account'
+    })
+
+    const menus = document.querySelectorAll('.menu-btn-items')
+    const accountBtnWrapper = document.createElement('div')
+    accountBtnWrapper.classList.add('account-btn-wrapper')
+
+    const mobileMenuBtn = document.createElement('div')
+    mobileMenuBtn.classList.add('account-btn-wrapper')
+    mobileMenuBtn.style.flexGrow = 1
+    mobileMenuBtn.innerHTML = `
        <a href="https://platform.unbench.us/account" target="_self" class="account-btn btn flex-hor w-inline-block" style="justify-content: center">
          <img src="https://cdn.jsdelivr.net/gh/andrii-h/unbench-sso/img/Profile-white.svg" loading="lazy" alt="" class="profile-icon">
          <div class="subtitle white-text">Account</div>
        </a>
      `
 
-  menus.forEach(i => {
-    const btn = i.innerHTML
-    i.innerHTML = ''
-    i.classList.add('account-btn-position')
-    accountBtnWrapper.innerHTML = `
+    menus.forEach(i => {
+      const btn = i.innerHTML
+      i.innerHTML = ''
+      i.classList.add('account-btn-position')
+      accountBtnWrapper.innerHTML = `
 ${btn}
        <ul class="account-btn-sublist">
          <li class="account-btn-sublist-item">
            <div class="account-btn-sublist-name">
              <div class="account-btn-sublist-name-icon">${data?.username
-      ? data.username[0]
-      : ''}</div>
+        ? data.username[0]
+        : ''}</div>
              <div>${data?.username || 'Account'}</div>
            </div>
          </li>
@@ -146,7 +149,8 @@ ${btn}
          </li>
        </ul>
      `
-    i.appendChild(accountBtnWrapper.cloneNode(true))
-  })
-  document.querySelector('.menu-btn-wrapper').appendChild(mobileMenuBtn)
+      i.appendChild(accountBtnWrapper.cloneNode(true))
+    })
+    document.querySelector('.menu-btn-wrapper').appendChild(mobileMenuBtn)
+  }
 }
